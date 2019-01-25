03ed0b1df48efe358cc00bb67c93a2f18187cc09
Thomas Michot <michot.th@gmail.com>
2019-01-25 15:01:18 +0100

    liste des evenements valide

--- js/eventvalide.js
+++ js/eventvalide.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-            $("#decl").click(function(event) {
-                $.getJSON('http://localhost:8080/event/1' , function(ev){
-                    $('#eventvalide').html('<p> Id : ' + ev.idEvent + '</p>');
-                    $('#eventvalide').append('<p> date de début : ' + ev.dateDebut + '</p>')
-                    $('#eventvalide').append('<p> date de fin : ' + ev.dateFin + '</p>')
-                    $('#eventvalide').append('<p> nombre de participants : ' + ev.nbUser + '</p>')
-                    $('#eventvalide').append('<p> statut : ' + ev.status + '</p>')
-                });
-            });
-        });
+        $.getJSON('http://localhost:8080/events' , function(evs){
+              
+            for (i in evs){
+            	if (evs[i].status == 1)
+                $('#elem').append('<tr><form  action="/event" method="POST"><td>' + evs[i].idEvent + '</td><td>' + evs[i].dateDebut + ' à ' + evs[i].dateFin + '</td><td>' + evs[i].nbUser+ '</td><td><input type="submit" value="afficher votre lunch" class="btn btn-outline-secondary"></td></form></tr>');
+            }
+            
+          });
+        
+           // $('#elem').html('<tr><td>1text</td><td>2text</td><td>3text</td></tr>');
+                 
+          
+      });
+
+      $(document).ready(function() {
+});
